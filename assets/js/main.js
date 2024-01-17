ec80cd13f6db7a890a866d66f038d51cd9158b52
Lindemberg <lindembergparaiba@gmail.com>
2024-01-16 22:49:10 -0300

    implementacao do grafico e inicio da estilizacao

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,30 @@ monthsSlider.addEventListener("input", () => {
   monthsSpan.textContent =
     updatedMonths === 1 ? "1 mês" : updatedMonths + " meses";
 });
+
+// inicio do perrengue com os gráficos
+
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = document.getElementById("myChart");
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
